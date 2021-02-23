127c28ed06463c5486180e7c1fc13fca1aecd65a
Mariana Lira <marilira2006@gmail.com>
2021-02-23 14:07:43 -0300

    Class 2

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import { CompleteChallenges } from '../components/CompleteChallenges';
+import { Countdown } from '../components/CountDown';
+import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | Move.it</title>
+      </Head>
+      
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompleteChallenges />
+          <Countdown />
+        </div>
+
+        <div>
+
+        </div>
+      </section>
+    </div>
+  )
+}
